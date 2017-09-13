6b364a83f1bc28b812f077e2ee0644d7043baf75
Zach Lott <zacharylott94@gmail.com>
2017-09-12 23:51:43 -0500

    +added a reset button to the HTML for testing +added a global context object +"data" is now "taskList" which is now "context.tasks". refer to it as the latter +added a currentDate and lastDate to the context. it is unused as of now +added function nextDay. It is untested +added function resetTasks. it is being tested. It appears to work Things are being reworked to ensure the global context is updated properly and not just local function copies.

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let data = []
+//global context object for holding our precious data
+let context = {};
+
+let taskList = []
 
 //bogus test data
-data.push({
+taskList.push({
   text: "a nisi praesentibus",
   state: false,
   element: ""
           });
-data.push({
+taskList.push({
   text: "voluptatibus si aut",
   state: false,
   element: ""
           });
+context.tasks = taskList;
+//taskList should not be referred to past this point
+//refer to context.tasks instead
+
+//get a date object to keep track of time
+context.currentDate = new Date;
+//populate lastDate
+context.lastDate = context.currentDate
 
 
 function createTask(task) {
@@ -25,28 +36,39 @@ function createTask(task) {
   task.element = document.createElement("div");
   let check = document.createElement("button");
   check.state = false;
-  check.textContent = "0";
+  check.textContent = "false";
   let span = document.createElement("span");
   span.textContent = task.text;
   let del = document.createElement("button");
   del.textContent = "D";
   
   check.onclick = () => {
+    
+    
     console.log("state for \"" + task.text + "\" was " + task.state);
+    
+    //flip state
     task.state = !task.state;
+    
     console.log("it is now " + task.state);
+    
+    //update button text
+    check.textContent = task.state;
+    
+    //testing stuff here
+    console.log(context.tasks);
   }
   
   del.onclick = () => {
     console.log("You pushed the delete button!");
     //grab the index of the task
-    let index = data.indexOf(task);
+    let index = context.tasks.indexOf(task);
     //remove it from our global
     if (index > -1) {
-      data.splice(index, 1);
+      context.tasks.splice(index, 1);
     }
     //testing stuff here
-    console.log(data);
+    console.log(context.tasks);
     //remove the dom element of task from the list
     list.removeChild(task.element);
     //trash task
@@ -60,7 +82,23 @@ function createTask(task) {
   list.appendChild(task.element);
 }
 
+function nextDay(current, last) {
+  if (current != last) {
+    last = current;
+    resetTasks(context.tasks);
+  }
+}
+
+function resetTasks() {
+  console.log("tasks resetting")
+  for (let each in context.tasks) {
+    context.tasks[each].state = false;
+  }
+  console.log(context.tasks);
+}
+
+
 //populate HTML
-for (let each in data) {
-  createTask(data[each]);
+for (let each in context.tasks) {
+  createTask(context.tasks[each]);
 }
