f2c5af301fd4dacf65a60832e8b24336e94b898d
Zach Lott <zacharylott94@gmail.com>
2017-09-13 13:58:43 -0500

    +changed the functionality of the add task button first iteration of script.js +added several empty functions +implemented createTask. it creates an object +implemented updateDoc. it populates the doc based on the contents of context. Basic functionality at this point. Tasks can be added, but they aren't saved and their buttons do not work.

--- script.js
+++ script.js
@@ -1,140 +1,92 @@
-//global context object for holding our precious data
+//global context
 let context = {};
-
 context.tasks = [];
-//taskList should not be referred to past this point
-//refer to context.tasks instead
+context.currentTime = 0;
+context.lastTime = 0;
+context.clockElement = document.getElementById("clock");
+context.listElement = document.getElementById("list");
+context.inputElement = document.getElementById("input");
 
-//get a date object to keep track of time
-context.currentDate = new Date;
-//populate lastDate
-context.lastDate = Date.now();
-context.clock = document.getElementById("clock");
-
-
-//this function takes an object
-function createTask(task) {
-  /*if no task, assume the user is creating one and grab the input element*/
-  task = task || {text:document.getElementById("input").value,
-                 state:false};
+//syncs context with the document. doesn't return
+function updateDoc(){
   
-  
-  /*Define stuff
-    1. list is our <main> with id="list"
-    2. task is the line that spells out the task and has buttons
-    3. check is our checkbox button
-    4. span is where our text goes
-    5. del is our delete button*/
-  let list = document.getElementById("list");
-  task.element = document.createElement("div");
+  function buildTaskElement(task){
   let check = document.createElement("button");
-  check.state = false;
-  check.textContent = "false";
-  task.checkbox = check;
+  let del = document.createElement("button");
   let span = document.createElement("span");
   span.textContent = task.text;
+  check.textContent = "false";
+  del.textContent = "delete";
+  let element = document.createElement("div");
+  element.appendChild(check);
+  element.appendChild(span);
+  element.appendChild(del);
+  return element;
+}
+  
+  context.listElement.innerHTML = "";
+  
+  for(let each in context.tasks){
+    context.listElement.appendChild(buildTaskElement(context.tasks[each]));
+  }
+}
+
+//keeps time. Doesn't return, calls update
+function clock(){}
+
+//compares current and last times, returns bool
+function nextday(){}
+
+//takes text. If no text, takes text from context.inputElement returns an object
+function createTask(input){
+  input = input || context.inputElement
+  console.log(input);
+  //create an empty object
+  let task = {};
+  //throw in the text from our input box
+  task.text = input.value;
+  //set input box to empty
+  input.value = "";
+  //give our object a state
+  task.state = false;
+  return task;
+}
+
+//takes a task object, and builds the DOM elements for it.
+//returns object with .element for DOM
+function buildTaskElement(task){
+  let check = document.createElement("button");
   let del = document.createElement("button");
-  del.textContent = "D";
-  
-  
-  check.onclick = () => {
-    
-    
-    console.log("state for \"" + task.text + "\" was " + task.state);
-    
-    //flip state
-    task.state = !task.state;
-    
-    console.log("it is now " + task.state);
-    
-    //update button text
-    update();
-    
-    //testing stuff here
-    console.log(context.tasks);
-  }
-  
-  del.onclick = () => {
-    console.log("You pushed the delete button!");
-    //grab the index of the task
-    let index = context.tasks.indexOf(task);
-    //remove it from our global
-    if (index > -1) {
-      context.tasks.splice(index, 1);
-    }
-    //testing stuff here
-    console.log(context.tasks);
-    //remove the dom element of task from the list
-    list.removeChild(task.element);
-    //trash task
-    task = null;
-    dump();
-  }
-  
-  /*Chain everything together*/
-  task.element.appendChild(check);
-  task.element.appendChild(span);
-  task.element.appendChild(del);
-  list.appendChild(task.element);
-  context.tasks.push(task)
+  let span = document.createElement("span");
+  span.textContent = task.text;
+  check.textContent = "false";
+  del.textContent = "delete";
+  let element = document.createElement("div");
+  element.appendChild(check);
+  element.appendChild(span);
+  element.appendChild(del);
+  return element;
 }
 
-function resetTasks() {
-  console.log("tasks resetting")
-  for (let each in context.tasks) {
-    context.tasks[each].state = false;
-  }
-  update();
-  console.log(context.tasks);
-  
+//grabs cookie, returns object
+function grab(cookie){}
+
+//dumps context object. returns string
+function dump(){}
+
+//takes an element and appends it to the list
+function appendList(element){
+  context.listElement.appendChild(element);
 }
 
-function update(){
-  for (let each in context.tasks){
-    context.tasks[each].checkbox.textContent = context.tasks[each].state;
-  }
-}
+let test = createTask({value:"test"});
+context.tasks.push(test);
+let test2 = createTask({value:"test2"});
+context.tasks.push(test2);
 
-function clock(){
-  context.currentDate = new Date();
-  context.clock.textContent = context.currentDate.toLocaleTimeString();
-  try
-  {
-    if (context.currentDate.getDay()!= context.lastDate.getDay()) {
-      context.lastDate = context.currentDate;
-      resetTasks();
-    }
-  }
-  catch(e){
-  }
-}
-
-function dump(){
-  document.cookie = null;
-  document.cookie = JSON.stringify(context);
-  console.log(document.cookie);
-}
-
-function grab(){
-  context = JSON.parse(document.cookie);
-  context.lastDate = new Date(context.lastDate);
-  //populate HTML
-  for (let each in context.tasks) {
-    createTask(context.tasks[each]);
-  }
-  //I have to do this because the parse is trashing my element link in clock for some reason
-  context.clock = document.getElementById("clock");
-  
-
-  
-}
-
-function emptyTasks(){
-  context.tasks = [];
-  document.cookie = JSON.stringify(context);
-}
-grab()
-
-//update current date every second
-setInterval(clock,1000);
-
+updateDoc();
+//updates stuff
+setInterval(() =>{
+  clock();
+  updateDoc();
+}, 100);
